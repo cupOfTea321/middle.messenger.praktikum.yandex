b37cf3c8913162d820dca95e152094844303c859
Alex <ddragon99999@gmail.com>
2023-09-12 10:30:43 +0300

    [global] api + controllers

--- vite-messenger/src/pages/profile/index.ts
+++ vite-messenger/src/pages/profile/index.ts
@@ -1,12 +1,26 @@
 import Block from '../../utils/Block';
 import template from "./profile.hbs";
+import {withStore} from "../../utils/Store";
+import {User} from "../../api/AuthAPI";
 
 interface ChatPageProps {
     title: string;
 }
-
+const userFields = [
+    'id',
+    'first_name',
+    'second_name',
+    'display_name',
+    'login',
+    'avatar',
+    'email',
+    'phone'
+] as Array<keyof ProfileProps>;
+interface ProfileProps extends User {}
 export class ProfilePage extends Block {
     render() {
         return this.compile(template, this.props);
     }
 }
+
+const withUser = withStore((state) => ({ ...state.user }))

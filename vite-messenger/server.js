0951962f943cf351c2e440e921bac7d3e0af33d7
Alex <ddragon99999@gmail.com>
2023-09-22 12:46:25 +0300

    [server] express middleware

--- vite-messenger/server.js
+++ vite-messenger/server.js
@@ -1,42 +1,20 @@
-// const express = require('express');
-//
-// const PORT = 3000;
-// const app = express();
-// app.use(express.static(`${__dirname}/dist`));
-// // app.use(express.static(__dirname + '/dist'));
-//
-// app.listen(PORT, () => {
-//   // console.log(`Мой текст в логе после запуска ${PORT}!`);
-// });
-import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import fallback from "express-history-api-fallback";
+import express from "express";
 
 const __filename = fileURLToPath(import.meta.url);
+
 const __dirname = path.dirname(__filename);
 
+
 const app = express();
+const root =`${__dirname}/dist`
 const PORT = 3000;
 
-app.use(express.static(__dirname + '/dist/'));
+app.use(express.static(root));
+app.use(fallback("index.html", { root }));
 
-
-app.listen(PORT, function () {
+app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}!`);
 });
-
-// const express = require('express')
-// const app = express()
-// const port = 3000
-//
-// app.get('/', (request, response) => {
-//     response.send('Hello from Express!')
-// })
-//
-// app.listen(port, (err) => {
-//     if (err) {
-//         return console.log('something bad happened', err)
-//     }
-//
-//     console.log(`server is listening on ${port}`)
-// })
